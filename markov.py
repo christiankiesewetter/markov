7e55be8192d07c7421311659a6c090e2e3725a18
christiankiesewetter <corkscrew.ki@gmail.com>
2021-12-30 22:38:16 +0100

    sparse calc

--- markov.py
+++ markov.py
@@ -14,7 +14,7 @@ class MarkovModel:
     def __init__(self, **kwargs):
         self.config.update(kwargs)
     
-    def calc_freqs_for_word(self, word, seq, order):
+    def calc_freqs_for_word(self, word, seq, order, epsilon):
         picks = np.array([False for _ in range(len(seq))])
         for pos in range(0, len(seq)):
             if ' '.join(seq[pos:pos+order]) == ' '.join(word) and (pos+order < len(picks)):
@@ -22,9 +22,11 @@ class MarkovModel:
         
         words, counts = np.unique(seq[picks], return_counts=True)
         words = np.array([self.vocab['word'][w] for w in words])
-        return words, counts
+        freq = np.log(counts  + epsilon / counts.sum())
+        return dict(zip(words, freq))
 
     def setup_n_order_vocab(self, sequences, order):
+        sequences = set(sequences)
         words = [m for m in product(sequences, repeat = order)]
         id2w = dict(enumerate(words))
         w2id = {w:id for id, w in id2w.items()}
@@ -44,22 +46,24 @@ class MarkovModel:
         inputs = len(self.vocab['id'])
         self.pi_i = np.ones_like((inputs)) * epsilon + self.__rel_probs
 
-    def train_n_order(self, allsequences, order, epsilon):
-        inputs1 = len(self.__vocab[f'id2w{order}'])
-        outputs = len(self.vocab['id'])
-        A_ij = np.ones((inputs1, outputs)) * epsilon
+    def train_n_order(self, sequence, order, epsilon):
+        self.A_ij.update({order:dict()})
         for w, id in self.__vocab[f'w2id{order}'].items():
-            wm, cm = self.calc_freqs_for_word(w, allsequences, order)
-            a_j = np.zeros(outputs)
-            if len(wm != 0):
-                np.put(a_j, wm, cm)
-            A_ij[id] = a_j
-
-        self.A_ij.update({order: np.log(A_ij + epsilon) / outputs})
+            sparse_dict = self.calc_freqs_for_word(w, sequence, order, epsilon)
+            if len(sparse_dict) > 0:
+                self.A_ij[order].update({id : sparse_dict})
 
 
     def train(self, sequences, order, epsilon = 1e-10):
-        allsequences = np.array([unchar.sub(r'', word).lower() for line in sequences for word in line.split() if len(line.strip())>0 and len(word.strip())>0])
+        allsequences = []
+        for line in sequences: 
+            for word in line.split():
+                if len(line.strip())>0 and len(word.strip())>0:
+                    text = unchar.sub(r'', word).lower()
+                    if text.strip() != '':
+                        allsequences.append(text)
+        
+        allsequences = np.array(allsequences)
 
         self.setup_vocab(allsequences)
         self.train_frequency_probabilites(epsilon)
@@ -75,7 +79,7 @@ class MarkovModel:
         seq = begin.lower().split(' ')
         in_id = self.__vocab[f'w2id{order}'][tuple(seq[:order])]
         for step in range(nsteps):
-            out_id = np.argmax(self.A_ij[order][in_id])
+            out_id = sorted(self.A_ij[order][in_id].items(), key = lambda m: m[1])[0][0]
             seq.append(self.vocab['id'][out_id])
             in_id = self.__vocab[f'w2id{order}'][tuple(seq[step+1:step+order+1])]
         return ' '.join(seq)
@@ -86,5 +90,5 @@ if __name__ == '__main__':
     with open('texts2.txt','r',encoding='utf8') as f:
         texts = [line for line in f.read().split('\n') if len(line.strip())]
     mm = MarkovModel()
-    mm.train(texts, order = 2, epsilon = 0)
+    mm.train(texts, order = 2)
     print(mm('es waren', order = 2, nsteps = 30))
