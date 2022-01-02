2cbc25478fe0b93be23e44752e08153e44f7ff3a
christiankiesewetter <corkscrew.ki@gmail.com>
2022-01-02 12:52:41 +0100

    hidden markov

--- markov_hidden.py
+++ markov_hidden.py
@@ -14,17 +14,19 @@ class MarkovModel:
     __vocab = dict()
     pi_i = dict()
     A_ij = dict()
-    vocab = dict()
+    B_ij = dict()
     __overall_length = 0
     epsilon = 1e-04
-    
+    ################################################
+    ## initializing and storing, retrieving model ##
+    ################################################
     def __init__(self, model_path = None, epsilon = None):
         if not None is model_path and os.path.isfile(model_path):
             with open(model_path, 'rb') as mfile:
                 model = pickle.load(mfile)
                 self.A_ij = model['Aij']
-                self.__vocab = model['internal_vocab'] 
-                self.vocab = model['external_vocab']
+                self.B_ij = model['Bij']
+                self.__vocab = model['internal_vocab']
                 self.pi_i = model['pi_i']
                 print('model loaded')
 
@@ -36,26 +38,37 @@ class MarkovModel:
         with open(file = f'{model_path}', mode = 'wb') as mfile:
             model = dict(
                 Aij = self.A_ij,
+                Bij = self.B_ij,
                 internal_vocab = self.__vocab,
-                external_vocab = self.vocab,
-                pi_i = self.pi_i
-            )
+                pi_i = self.pi_i)
             pickle.dump(model, mfile)
             print('model stored')
-        
-    def calc_freqs_for_word(self, word, seq, order):
+
+
+    ################################################
+    ##          Vocabulary Building Task          ##
+    ################################################
+    def tokenize(self, sequences):
+        tokenized_sequences = []
+        for sequence in sequences:
+            cleaned_sequence = unchar.sub(r'', sequence).lower().split()
+            tokenized_sequence = [self.__vocab['w2id1'][w] for w in cleaned_sequence]
+            tokenized_sequences.append(tokenized_sequence)
+        return tokenized_sequences
+
+    def calc_outgoing_freqs_for_word(self, word, seq, order):
         picks = np.array([False for _ in range(len(seq))])
         for pos in range(0, len(seq)-order):
             if ' '.join(seq[pos:pos+order]) == word:
                 picks[pos+order] = True
         
-        words, counts = np.unique(seq[picks], return_counts=True)
-        words = np.array([self.vocab['word'][w] for w in words])
+        words, counts = np.unique(np.array(seq)[picks], return_counts=True)
+        words = np.array([self.__vocab['w2id1'][w] for w in words])
         freq = counts / (counts.sum() + self.epsilon)
-        return dict(zip(words, freq))
+        return tuple(zip(words, freq))
 
     def setup_n_order_vocab(self, sequences, order):
-        words = set((' '.join(sequences[pos:pos+order]) for pos in range(len(sequences) - order)))
+        words = set((' '.join(sequence[pos:pos+order]) for sequence in sequences for pos in range(len(sequence) - order + 1)))
         id2w = dict(enumerate(words))
         w2id = {w:id for id, w in id2w.items()}
         self.__vocab.update({
@@ -63,93 +76,99 @@ class MarkovModel:
             f'w2id{order}' : w2id
         })
 
-    def setup_vocab(self, allsequences):
+    def setup_external_vocab(self, allsequences):
         words, counts = np.unique(allsequences, return_counts=True)
         self.__overall_length = counts.sum()
-        self.__rel_probs = counts/self.__overall_length
+        self.__word_frequency = counts/self.__overall_length
         self.vocab['id'] = dict(enumerate(words))
         self.vocab['word'] = {v:k for k,v in self.vocab['id'].items()}
 
+    ##############################################
+    ##              Training Tasks              ##
+    ##############################################
     def print_progress(self, progress, order, width = 100):
         bars = '*' * int(progress * width)
         print(f'{bars: <{width}} {progress * 100:.2f}%, order {order}', end='\r')
         if progress == 1:
             print('\n')
 
+    def clean_sequences(self, sequences):
+        allsequences = []
+        for line in sequences:
+            truncated_line = line.strip()
+            if len(truncated_line)>0:
+                splitline = unchar.sub(r'', truncated_line.lower()).split()
+                allsequences.append(splitline)
+        return allsequences
 
-    def train_n_order(self, sequence, order):
-        self.A_ij.update({order:dict()})
+
+    def train_n_order(self, sequences, order):
         inputs = len(self.__vocab[f'w2id{order}'])
+        outputs = len(self.__vocab[f'w2id1'])
+        self.A_ij.update({order:np.zeros(shape = (inputs, outputs))})
         for num, (w, id) in enumerate(self.__vocab[f'w2id{order}'].items()):
-            sparse_dict = self.calc_freqs_for_word(w, sequence, order)
-            if len(sparse_dict) > 0:
-                self.A_ij[order].update({id : sparse_dict})
+            for sequence in sequences:
+                sparse_outgoing_dict = self.calc_outgoing_freqs_for_word(w, sequence, order)
+                for word_id, freq in sparse_outgoing_dict:
+                    self.A_ij[order][self.__vocab[f'w2id{order}'][w],word_id] = freq / len(sequences)
             self.print_progress((num + 1) / inputs, order)
 
     def train(self, sequences, orders):
-        allsequences = []
-        intermediate_pi_i = dict()
-        actual_lines = 0
-        for line in sequences: 
-            splitline = line.split()
-            for word in splitline:
-                if len(line.strip())>0 and len(word.strip())>0:
-                    isfirstword = True
-                    text = unchar.sub(r'', word)
-                    if text.strip() != '':
-                        entry = text.lower().strip()
-                        allsequences.append(entry)
-                        if isfirstword:
-                            intermediate_pi_i[entry] = intermediate_pi_i.get(entry, 0) + 1
-                            actual_lines += 1
-                            isfirstword = False
-
-        self.pi_i = {k:(v/actual_lines) for k, v in intermediate_pi_i.items()}
-        allsequences = np.array(allsequences)
-
-        self.setup_vocab(allsequences)
-        
+        all_sequences = self.clean_sequences(sequences)
         for order in orders:
-            self.setup_n_order_vocab(sequences = allsequences, order = order)
-            self.train_n_order(allsequences, order = order)
+            self.setup_n_order_vocab(sequences = all_sequences, order = order)
+            self.train_n_order(sequences = all_sequences, order = order)
+        self.pi_i = np.zeros(len(self.__vocab['w2id1']))
+
 
     def __len__(self):
         return self.__overall_length
     
+    ##############################################
+    ##                 Inference                ##
+    ##############################################
+    
     def next_id(self, order, wkey):
         curr_order = order
         curr_wkey = wkey
-
-        while not curr_wkey in self.__vocab[f'w2id{curr_order}'] and curr_order > 0:
+        while curr_order > 0 and not curr_wkey in self.__vocab[f'w2id{curr_order}']:
             curr_order -= 1
             curr_wkey = ' '.join(curr_wkey.split(' ')[-curr_order:])
 
         if curr_order > 0:
             in_id = self.__vocab[f'w2id{curr_order}'][curr_wkey]
         else:
-            in_id = int(random.random() * len(self.__vocab[f'w2id1']))
-        
+            in_id = self.process_sample([(self.vocab['word'][k], v) for k,v in self.pi_i.items()])
         return in_id, curr_order
-    
-    def sample_word(self,order, in_id):
+
+    def process_sample(self, sampleitems):
         p = random.random()
         if p <= self.epsilon: # Case, when very low values are accepted, we randomly choose something
             out_word = random.choice(list(self.vocab['id'].values()))
             return out_word
-
-        sortedwords = sorted(self.A_ij[order][in_id].items(), key = lambda m: np.log(m[1]), reverse = True)
+        
+        sortedwords = sorted(sampleitems, key = lambda m: np.log(m[1]), reverse = True)
         out_id = sortedwords[-1][0] # default value
         for (iid, prob) in sortedwords:
             if prob >= p:
                 out_id = iid
                 break
+        return out_id
+
+
+    def sample_word(self, order, in_id):
+        out_id = self.process_sample(self.A_ij[order][in_id].items())
         return self.vocab['id'][out_id]
+
 
     def generate(self, begin, order, nsteps = 10):
         seq = begin.lower().split(' ')
         wkey = ' '.join(seq[-order:])
         in_id, curr_order = self.next_id(order, wkey)
-        
+        if curr_order == 0:
+            curr_order = 1
+            seq[0] = self.vocab['id'][in_id]
+
         for step in range(nsteps):
             next_word = self.sample_word(curr_order, in_id)
             seq.append(next_word)
@@ -159,7 +178,7 @@ class MarkovModel:
         return ' '.join(seq)
     
 
-    def __call__(self, sequence, order):
+    def probability_for_sequence(self, sequence, order):
         '''
         Calculates the probability of the occuring sequence.
         '''
@@ -181,17 +200,18 @@ class MarkovModel:
 
             prob_sequence.append(ProbSeq(step, prob, curr_order))
         
-        return np.prod([a.prob for a in prob_sequence]), prob_sequence
+        return prob_sequence
 
+
+    def __call__(self, sequence, order):
+        prob_sequence = self.probability_for_sequence(sequence, order)
+        return np.prod([a.prob for a in prob_sequence])
 
 
 if __name__ == '__main__':
-    with open('texts.txt','r',encoding='utf8') as f:
+    with open('texts2.txt','r',encoding='utf8') as f:
         texts = [line for line in f.read().split('\n') if len(line.strip())]
     
-    mm = MarkovModel('mm1.dat', epsilon = 1e-03)
-    #mm.train(texts, orders = [1,2])
-    #mm.save('mm1.dat')
-
-    print(mm.generate('Es waren', order = 4, nsteps = 50))
-    print(mm('es waren zweiundzwanzig mark und faltete', order = 2)[0])
+    mm = MarkovModel(epsilon = 1e-03)
+    mm.train(texts, orders = [1,2,3])
+    mm.save('hmm1.dat')
--- markov_small_dict.py
+++ markov_small_dict.py
@@ -14,17 +14,17 @@ class MarkovModel:
     __vocab = dict()
     pi_i = dict()
     A_ij = dict()
-    vocab = dict()
     __overall_length = 0
     epsilon = 1e-04
-    
+    ################################################
+    ## initializing and storing, retrieving model ##
+    ################################################
     def __init__(self, model_path = None, epsilon = None):
         if not None is model_path and os.path.isfile(model_path):
             with open(model_path, 'rb') as mfile:
                 model = pickle.load(mfile)
                 self.A_ij = model['Aij']
-                self.__vocab = model['internal_vocab'] 
-                self.vocab = model['external_vocab']
+                self.__vocab = model['internal_vocab']
                 self.pi_i = model['pi_i']
                 print('model loaded')
 
@@ -37,25 +37,35 @@ class MarkovModel:
             model = dict(
                 Aij = self.A_ij,
                 internal_vocab = self.__vocab,
-                external_vocab = self.vocab,
-                pi_i = self.pi_i
-            )
+                pi_i = self.pi_i)
             pickle.dump(model, mfile)
             print('model stored')
-        
-    def calc_freqs_for_word(self, word, seq, order):
+
+
+    ################################################
+    ##          Vocabulary Building Task          ##
+    ################################################
+    def tokenize(self, sequences):
+        tokenized_sequences = []
+        for sequence in sequences:
+            cleaned_sequence = unchar.sub(r'', sequence).lower().split()
+            tokenized_sequence = [self.__vocab['w2id1'][w] for w in cleaned_sequence]
+            tokenized_sequences.append(tokenized_sequence)
+        return tokenized_sequences
+
+    def calc_outgoing_freqs_for_word(self, word, seq, order):
         picks = np.array([False for _ in range(len(seq))])
         for pos in range(0, len(seq)-order):
             if ' '.join(seq[pos:pos+order]) == word:
                 picks[pos+order] = True
         
-        words, counts = np.unique(seq[picks], return_counts=True)
-        words = np.array([self.vocab['word'][w] for w in words])
+        words, counts = np.unique(np.array(seq)[picks], return_counts=True)
+        words = np.array([self.__vocab['w2id1'][w] for w in words])
         freq = counts / (counts.sum() + self.epsilon)
-        return dict(zip(words, freq))
+        return tuple(zip(words, freq))
 
     def setup_n_order_vocab(self, sequences, order):
-        words = set((' '.join(sequences[pos:pos+order]) for pos in range(len(sequences) - order)))
+        words = set((' '.join(sequence[pos:pos+order]) for sequence in sequences for pos in range(len(sequence) - order + 1)))
         id2w = dict(enumerate(words))
         w2id = {w:id for id, w in id2w.items()}
         self.__vocab.update({
@@ -63,63 +73,61 @@ class MarkovModel:
             f'w2id{order}' : w2id
         })
 
-    def setup_vocab(self, allsequences):
+    def setup_external_vocab(self, allsequences):
         words, counts = np.unique(allsequences, return_counts=True)
         self.__overall_length = counts.sum()
-        self.__rel_probs = counts/self.__overall_length
+        self.__word_frequency = counts/self.__overall_length
         self.vocab['id'] = dict(enumerate(words))
         self.vocab['word'] = {v:k for k,v in self.vocab['id'].items()}
 
+    ##############################################
+    ##              Training Tasks              ##
+    ##############################################
     def print_progress(self, progress, order, width = 100):
         bars = '*' * int(progress * width)
         print(f'{bars: <{width}} {progress * 100:.2f}%, order {order}', end='\r')
         if progress == 1:
             print('\n')
 
+    def clean_sequences(self, sequences):
+        allsequences = []
+        for line in sequences:
+            truncated_line = line.strip()
+            if len(truncated_line)>0:
+                splitline = unchar.sub(r'', truncated_line.lower()).split()
+                allsequences.append(splitline)
+        return allsequences
 
-    def train_n_order(self, sequence, order):
-        self.A_ij.update({order:dict()})
+
+    def train_n_order(self, sequences, order):
         inputs = len(self.__vocab[f'w2id{order}'])
+        outputs = len(self.__vocab[f'w2id1'])
+        self.A_ij.update({order:np.zeros(shape = (inputs, outputs))})
         for num, (w, id) in enumerate(self.__vocab[f'w2id{order}'].items()):
-            sparse_dict = self.calc_freqs_for_word(w, sequence, order)
-            if len(sparse_dict) > 0:
-                self.A_ij[order].update({id : sparse_dict})
+            for sequence in sequences:
+                sparse_outgoing_dict = self.calc_outgoing_freqs_for_word(w, sequence, order)
+                for word_id, freq in sparse_outgoing_dict:
+                    self.A_ij[order][self.__vocab[f'w2id{order}'][w],word_id] = freq / len(sequences)
             self.print_progress((num + 1) / inputs, order)
 
     def train(self, sequences, orders):
-        allsequences = []
-        intermediate_pi_i = dict()
-        actual_lines = 0
-        for line in sequences: 
-            splitline = line.split()
-            for word in splitline:
-                if len(line.strip())>0 and len(word.strip())>0:
-                    isfirstword = True
-                    text = unchar.sub(r'', word)
-                    if text.strip() != '':
-                        entry = text.lower().strip()
-                        allsequences.append(entry)
-                        if isfirstword:
-                            intermediate_pi_i[entry] = intermediate_pi_i.get(entry, 0) + 1
-                            actual_lines += 1
-                            isfirstword = False
-
-        self.pi_i = {k:(v/actual_lines) for k, v in intermediate_pi_i.items()}
-        allsequences = np.array(allsequences)
-
-        self.setup_vocab(allsequences)
-        
+        all_sequences = self.clean_sequences(sequences)
         for order in orders:
-            self.setup_n_order_vocab(sequences = allsequences, order = order)
-            self.train_n_order(allsequences, order = order)
+            self.setup_n_order_vocab(sequences = all_sequences, order = order)
+            self.train_n_order(sequences = all_sequences, order = order)
+        self.pi_i = np.zeros(len(self.__vocab['w2id1']))
+
 
     def __len__(self):
         return self.__overall_length
     
+    ##############################################
+    ##                 Inference                ##
+    ##############################################
+    
     def next_id(self, order, wkey):
         curr_order = order
         curr_wkey = wkey
-
         while curr_order > 0 and not curr_wkey in self.__vocab[f'w2id{curr_order}']:
             curr_order -= 1
             curr_wkey = ' '.join(curr_wkey.split(' ')[-curr_order:])
@@ -128,9 +136,7 @@ class MarkovModel:
             in_id = self.__vocab[f'w2id{curr_order}'][curr_wkey]
         else:
             in_id = self.process_sample([(self.vocab['word'][k], v) for k,v in self.pi_i.items()])
-        
         return in_id, curr_order
-    
 
     def process_sample(self, sampleitems):
         p = random.random()
@@ -169,7 +175,7 @@ class MarkovModel:
         return ' '.join(seq)
     
 
-    def __call__(self, sequence, order):
+    def probability_for_sequence(self, sequence, order):
         '''
         Calculates the probability of the occuring sequence.
         '''
@@ -191,17 +197,18 @@ class MarkovModel:
 
             prob_sequence.append(ProbSeq(step, prob, curr_order))
         
-        return np.prod([a.prob for a in prob_sequence]), prob_sequence
+        return prob_sequence
 
+
+    def __call__(self, sequence, order):
+        prob_sequence = self.probability_for_sequence(sequence, order)
+        return np.prod([a.prob for a in prob_sequence])
 
 
 if __name__ == '__main__':
-    with open('texts.txt','r',encoding='utf8') as f:
+    with open('texts2.txt','r',encoding='utf8') as f:
         texts = [line for line in f.read().split('\n') if len(line.strip())]
     
-    mm = MarkovModel('mm1.dat', epsilon = 1e-03)
-    #mm.train(texts, orders = [1,2,3])
-    #mm.save('mm1.dat')
-
-    print(mm.generate('Du', order = 3, nsteps = 50))
-    print(mm('es waren zweiundzwanzig', order = 2)[0])
+    mm = MarkovModel(epsilon = 1e-03)
+    mm.train(texts, orders = [1,2,3])
+    mm.save('hmm1.dat')
